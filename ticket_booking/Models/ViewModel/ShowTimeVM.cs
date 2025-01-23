f652d004464cb3257c1c8e24056a5ca274e8815a
Martin-Huỳnh <85268766+MartinDT-TH@users.noreply.github.com>
2025-01-23 22:13:38 +0700

    update showtimevm model

--- ticket_booking/Models/ViewModel/ShowTimeVM.cs
+++ ticket_booking/Models/ViewModel/ShowTimeVM.cs
@@ -1,9 +1,11 @@
-﻿namespace ticket_booking.Models.ViewModel
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ticket_booking.Models.ViewModel
 {
     public class ShowTimeVM
     {
         public List<ShowTime> TimeList { get; set; }
-        public List<Seat> SeatList { get; set; }
+        public List<SelectListItem> SeatList { get; set; }
         public Seat Seat { get; set; }
         public Movie Movie { get; set; }    
     }
